7ef9bad133d6f0b137e80dc948b77d5211dafb30
Peyman Arian <peymanarian@icloud.com>
2026-09-24 03:19:56 +0330

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = "labchart-5b091755b4";
+const C = "labchart-974b919013";
 const SHELL = ["./","./index.html","./manifest.webmanifest",
   "./icons/icon-192.png","./icons/icon-512.png","./icons/icon-180.png"];
 self.addEventListener("install", e => {
